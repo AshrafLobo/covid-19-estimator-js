01400b03a1d15e255bc7a6a874fb3033d1d34f1f
AshrafLobo <ashraflobo@gmail.com>
2020-04-11 00:25:36 +0300

    Complete challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,23 @@
-const covid19ImpactEstimator = (data) => data;
+const covid19ImpactEstimator = (data) => {
+  const covidData = {
+    data,
+    impact: {},
+    severeImpact: {}
+  };
+
+  const calcEstimation = (days, severe = false) => {
+    const impactGrp = severe ? ['severeImpact', 50] : ['impact', 10];
+    const impactData = covidData[impactGrp[0]];
+    const factor = 2 ** Math.floor(days / 3);
+
+    impactData.confirmedCases = covidData.data.reportedCases * impactGrp[1];
+    impactData.infectionsByRequestedTime = impactData.confirmedCases * factor;
+  };
+
+  calcEstimation(28);
+  calcEstimation(28, true);
+
+  return covidData;
+};
 
 export default covid19ImpactEstimator;
