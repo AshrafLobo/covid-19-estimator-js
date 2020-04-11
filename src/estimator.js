90c9c6743c68e9f93242fdd9639b41ddb1329e9f
AshrafLobo <ashraflobo@gmail.com>
2020-04-11 18:53:14 +0300

    Try long form code 5

--- src/estimator.js
+++ src/estimator.js
@@ -18,39 +18,40 @@ const covid19ImpactEstimator = (data) => {
       numberOfDays = covidData.data.timeToElapse;
   }
 
-  covidData.impact.currentlyInfected = covidData.data.reportedCases * 10;
-  covidData.severeImpact.currentlyInfected = covidData.data.reportedCases * 50;
+  const calcEstimation = (days, severe = false) => {
+    const impactGrp = severe ? ['severeImpact', 50] : ['impact', 10];
+    const impactData = covidData[impactGrp[0]];
+    const {
+      reportedCases,
+      totalHospitalBeds
+    } = covidData.data;
 
-  covidData.impact.infectionsByRequestedTime = covidData.impact.currentlyInfected
-    * (2 ** Math.trunc(numberOfDays / 3));
-  covidData.severeImpact.infectionsByRequestedTime = covidData.severeImpact.currentlyInfected
-    * (2 ** Math.trunc(numberOfDays / 3));
+    impactData.currentlyInfected = reportedCases * impactGrp[1];
+    impactData.infectionsByRequestedTime = impactData.currentlyInfected
+      * (2 ** Math.trunc(numberOfDays / 3));
+    impactData.severeCasesByRequestedTime = Math.trunc(impactData.infectionsByRequestedTime * 0.15);
+    impactData.hospitalBedsByRequestedTime = Math.trunc(totalHospitalBeds * 0.35
+      - impactData.severeCasesByRequestedTime);
+  };
 
-  covidData.impact.severeCasesByRequestedTime = Math.trunc(covidData
-    .impact.infectionsByRequestedTime * 0.15);
-  covidData.severeImpact.severeCasesByRequestedTime = Math.trunc(covidData
-    .severeImpact.infectionsByRequestedTime * 0.15);
+  calcEstimation(numberOfDays);
+  calcEstimation(numberOfDays, true);
 
-  covidData.impact.hospitalBedsByRequestedTime = Math.trunc(covidData.data.totalHospitalBeds * 0.35
-    - covidData.impact.severeCasesByRequestedTime);
-  covidData.severeImpact.hospitalBedsByRequestedTime = Math.trunc(covidData.data.totalHospitalBeds
-    * 0.35 - covidData.severeImpact.severeCasesByRequestedTime);
+  // covidData.impact.casesForICUByRequestedTime = Math.trunc(covidData
+  //   .impact.infectionsByRequestedTime * 0.05);
+  // covidData.severeImpact.casesForICUByRequestedTime = Math.trunc(covidData
+  //   .severeImpact.infectionsByRequestedTime * 0.05);
 
-  covidData.impact.casesForICUByRequestedTime = Math.trunc(covidData
-    .impact.infectionsByRequestedTime * 0.05);
-  covidData.severeImpact.casesForICUByRequestedTime = Math.trunc(covidData
-    .severeImpact.infectionsByRequestedTime * 0.05);
+  // covidData.impact.casesForVentilatorsByRequestedTime = Math.trunc(covidData
+  //   .impact.infectionsByRequestedTime * 0.02);
+  // covidData.severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(covidData
+  //   .severeImpact.infectionsByRequestedTime * 0.02);
 
-  covidData.impact.casesForVentilatorsByRequestedTime = Math.trunc(covidData
-    .impact.infectionsByRequestedTime * 0.02);
-  covidData.severeImpact.casesForVentilatorsByRequestedTime = Math.trunc(covidData
-    .severeImpact.infectionsByRequestedTime * 0.02);
-
-  covidData.impact.dollarsInFlight = Math.trunc((covidData.impact.infectionsByRequestedTime
-    * 0.65 * covidData.data.region.avgDailyIncomePopulation) / 30);
-  covidData.severeImpact.dollarsInFlight = Math.trunc((covidData
-    .severeImpact.infectionsByRequestedTime * 0.65
-    * covidData.data.region.avgDailyIncomeInUSD) / numberOfDays);
+  // covidData.impact.dollarsInFlight = Math.trunc((covidData.impact.infectionsByRequestedTime
+  //   * 0.65 * covidData.data.region.avgDailyIncomePopulation) / 30);
+  // covidData.severeImpact.dollarsInFlight = Math.trunc((covidData
+  //   .severeImpact.infectionsByRequestedTime * 0.65
+  //   * covidData.data.region.avgDailyIncomeInUSD) / numberOfDays);
 
   return covidData;
 };
