600b6dad6e997f05c24fe698fe028a7dba0b1d21
AshrafLobo <ashraflobo@gmail.com>
2020-04-11 13:56:54 +0300

    Complete challenge 3

--- src/estimator.js
+++ src/estimator.js
@@ -25,10 +25,14 @@ const covid19ImpactEstimator = (data) => {
     impactData.currentlyInfected = covidData.data.reportedCases * impactGrp[1];
     impactData.infectionsByRequestedTime = impactData.currentlyInfected
       * (2 ** Math.floor(days / 3));
-    impactData.severeCasesByRequestedTime = Math.floor((impactData.infectionsByRequestedTime
-      * 15) / 100);
-    impactData.hospitalBedsByRequestedTime = Math.floor((covidData.data.totalHospitalBeds
-      * 35) / 100) - impactData.severeCasesByRequestedTime + 1;
+    impactData.severeCasesByRequestedTime = Math.floor(impactData.infectionsByRequestedTime * 0.15);
+    impactData.hospitalBedsByRequestedTime = Math.floor(covidData.data.totalHospitalBeds
+      * 0.35) - impactData.severeCasesByRequestedTime + 1;
+    impactData.casesForICUByRequestedTime = Math.floor(impactData.infectionsByRequestedTime * 0.5);
+    impactData.casesForVentilatorsByRequestedTime = Math.floor(impactData.infectionsByRequestedTime
+      * 0.2);
+    impactData.dollarsInFlight = Math.floor((impactData.infectionsByRequestedTime * 0.65 * 1.5)
+      / days);
   };
 
   calcEstimation(numberOfDays);
